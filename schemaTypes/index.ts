f05113f1282ed7fe1d924d31aacb674277ee3f91
NateAra <nate.arafayne@appliedtechnology.se>
2024-09-02 18:10:19 +0200

    chore: export schema types

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,9 @@
-export const schemaTypes = []
+import { artistType } from "./artistType";
+import { eventType } from "./eventTypes";
+import { venueType } from "./venueType";
+
+export const schemaTypes = [
+  eventType,
+  artistType,
+  venueType
+]
