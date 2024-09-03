ab2b64d6cb4b2b9432bea535fa8b29b371a99a0c
NateAra <nate.arafayne@appliedtechnology.se>
2024-09-03 14:42:07 +0200

    feat: update eventType schema with new fields and validations

--- schemaTypes/eventTypes.ts
+++ schemaTypes/eventTypes.ts
@@ -1,36 +1,76 @@
+import {CalendarIcon} from '@sanity/icons'
 import {defineField, defineType} from 'sanity'
 
 export const eventType = defineType({
   name: 'event',
   title: 'Event',
   type: 'document',
+  icon: CalendarIcon,
+  groups: [
+    {name: 'details', title: 'Details'},
+    {name: 'editorial', title: 'Editorial'},
+  ],
   fields: [
     defineField({
       name: 'name',
       type: 'string',
+      group: ['details', 'editorial'],
     }),
     defineField({
       name: 'slug',
       type: 'slug',
+      group: 'details',
       options: {source: 'name'},
       validation: (rule) => rule.required().error('Required to generate a page on website'),
+      hidden: ({document}) => !document?.name,
     }),
     defineField({
       name: 'eventType',
       type: 'string',
+      group: 'details',
+      options: {
+        list: ['in-person', 'virtual'],
+        layout: 'radio',
+      },
+      validation: (rule) =>
+        rule.custom((value, context) => {
+          if (value === 'in-person' && !context?.document?.venue) {
+            return 'In-person events must have a venue'
+          }
+
+          if (value === 'virtual' && context.document?.venue) {
+            return 'Only in-person event can have a venue, Please remove the venue'
+          }
+
+          return true
+        }),
     }),
     defineField({
       name: 'date',
       type: 'datetime',
+      group: 'details',
     }),
     defineField({
       name: 'doorsOpen',
+      description: 'Number of minutes before the start time for admission',
       type: 'number',
+      initialValue: 60,
+      group: 'details',
     }),
     defineField({
       name: 'venue',
       type: 'reference',
       to: [{type: 'venue'}],
+      readOnly: ({value, document}) => !value && document?.eventType === 'virtual',
+      validation: (rule) =>
+        rule.custom((value, context) => {
+          if (value && context?.document?.eventType === 'virtual') {
+            return 'Only in-person event can have a venue'
+          }
+
+          return true
+        }),
+      group: 'details',
     }),
     defineField({
       name: 'headline',
@@ -40,15 +80,45 @@ export const eventType = defineType({
     defineField({
       name: 'image',
       type: 'image',
+      group: 'editorial',
     }),
     defineField({
       name: 'details',
       type: 'array',
       of: [{type: 'block'}],
+      group: 'editorial',
     }),
     defineField({
       name: 'tickets',
       type: 'url',
+      group: 'details',
     }),
   ],
+  preview: {
+    select: {
+      name: 'name',
+      venue: 'venue.name',
+      artist: 'headline.name',
+      date: 'date',
+      image: 'image',
+    },
+    prepare({name, venue, artist, date, image}) {
+      const nameFormatted = name || 'Untitled Event'
+      const dateFormatted = date
+        ? new Date(date).toLocaleDateString(undefined, {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+          })
+        : 'No Date'
+
+      return {
+        title: artist ? `${nameFormatted} (${artist})` : nameFormatted,
+        subtitle: venue ? `${dateFormatted} at ${venue}` : dateFormatted,
+        media: image ? image : CalendarIcon,
+      }
+    },
+  },
 })
