a43922cd773bb1b095f743fa397b48fbe6fe732d
NateAra <nate.arafayne@appliedtechnology.se>
2024-09-04 13:56:44 +0200

    feat: Add custom structure for event filtering and document type organization

---
+++ structure/index.ts
@@ -0,0 +1,22 @@
+import { CalendarIcon, PinIcon, UserIcon } from '@sanity/icons'
+import type { StructureResolver } from 'sanity/structure'
+
+export const structure: StructureResolver = (S) =>
+  S.list()
+    .id('root')
+    .title('Content')
+    .items([
+      S.listItem()
+        .title('Upcoming Events')
+        .schemaType('event')
+        .icon(CalendarIcon)
+        .child(S.documentList().title('Upcoming Events').filter(`date > now()`)),
+      S.listItem()
+        .title('Past Events')
+        .schemaType('event')
+        .icon(CalendarIcon)
+        .child(S.documentList().title('Past Events').filter(`date < now()`)),
+      S.divider(),
+      S.documentTypeListItem('artist').title('Artists').icon(UserIcon),
+      S.documentTypeListItem('venue').title('Venues').icon(PinIcon),
+    ])
